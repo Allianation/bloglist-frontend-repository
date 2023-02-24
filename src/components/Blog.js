9a2fb3b242f1f582f6903ee2d77d25f4e443a475
Sergio <salliana@technisys.com>
2023-02-24 01:01:40 -0300

    Testing React apps: Exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <div>
+          {blog.title} <button onClick={toggleVisibility}>view</button>
+        </div>
+      </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.title} <button onClick={toggleVisibility}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>{blog.likes}</div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
