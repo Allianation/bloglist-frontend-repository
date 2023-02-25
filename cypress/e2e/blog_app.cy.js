154bc7acd48cae90982f990275b07f39932981fa
Sergio <salliana@technisys.com>
2023-02-25 18:27:47 -0300

    Testing React apps: Exercise 5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,13 +1,45 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/tests/reset");
+
+    const user = {
+      username: "root",
+      password: "password",
+      name: "root",
+    };
+
+    cy.request("POST", "http://localhost:3003/api/users/", user);
+
     cy.visit("http://localhost:3000");
   });
 
-  it("Login form is shown", function () {
+  it("login form is shown", function () {
     cy.contains("Log in to application");
     cy.contains("username");
     cy.contains("password");
     cy.contains("login");
+  });
+
+  describe("Login", function () {
+    it("success with correct credentials", function () {
+      cy.get("input:first").type("root");
+      cy.get("input:last").type("password");
+      cy.contains("login").click();
+
+      cy.contains("root logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("input:first").type("root");
+      cy.get("input:last").type("wrong");
+      cy.contains("login").click();
+
+      cy.get(".error")
+        .should("contain", "invalid username or password")
+        .and("have.css", "color", "rgb(132, 32, 41)")
+        .and("have.css", "border-style", "solid");
+
+      cy.get("html").should("not.contain", "root logged in");
+    });
   });
 });
